3b3a22abf5f4c6eb0c4fdda2f93c9604c02d7022
damian1ilin <damian.franco.ilincheta@gmail.com>
2024-04-25 15:22:17 -0300

    7- Conectados señal y Funcion para contar
    Conectadas las funciones "count_new_instance" con la señal "button_down" de los generadores.

--- scenes/Menu/Menu.gd
+++ scenes/Menu/Menu.gd
@@ -8,8 +8,16 @@ func _ready():
 	# Inicializa el texto de "CountLabel" usando el texto
 	# Constante "INSTANCES_TEXT" más el valor de "instancesCount" como Texto
 	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
+	
+	var rojo = get_node("RedGenerator")
+	rojo.button_down.connect(count_new_instance)
+
+	var violeta = get_node("VioletGenerator")
+	violeta.button_down.connect(count_new_instance)
+
 	pass
 
 func count_new_instance():
 	instancesCount += 1
-	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
+	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)	
+
